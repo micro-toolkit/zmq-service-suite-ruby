1e059a13e0160eb4d097f4bfb2512551499e81e8
Pedro Januário <prnjanuario@gmail.com>
2014-07-03 15:56:49 +0100

    implemented spec helper module to execute a broker mock
    This will allows easy integration spec without external components

---
+++ spec/spec_broker_helper.rb
@@ -0,0 +1,52 @@
+require 'ffi-rzmq'
+require 'msgpack'
+
+module BrokerHelper
+
+  LOG_BROKER_INFO = false
+
+  def run_broker(endpoint)
+
+    Thread.new do
+      puts "broker running" if LOG_BROKER_INFO
+
+      Thread.handle_interrupt(RuntimeError => :immediate) do
+
+        begin
+          context = ZMQ::Context.create
+          socket = context.socket ZMQ::ROUTER
+          socket.setsockopt(ZMQ::LINGER, 0)
+          socket.connect(endpoint)
+          puts "broker connected" if LOG_BROKER_INFO
+
+          socket.recv_strings(frames = [])
+
+          puts "broker received #{frames}" if LOG_BROKER_INFO
+
+          if block_given?
+            msg = ZSS::Message.parse(frames)
+
+            yield(msg)
+
+            frames = msg.to_frames
+            socket.send_strings(frames)
+
+            puts "broker sending #{frames}" if LOG_BROKER_INFO
+          end
+
+          Thread.stop
+
+        rescue => e
+          puts "WTF => #{e}"
+        ensure
+          # You can write resource deallocation code safely.
+          puts "broker stoping" if LOG_BROKER_INFO
+          socket.close if socket
+          context.terminate if context
+        end
+      end
+
+    end
+
+  end
+end
