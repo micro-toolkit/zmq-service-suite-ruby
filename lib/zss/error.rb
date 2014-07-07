89a60e76d59e9f78b9bd30db498a1478bf075717
Pedro Januário <prnjanuario@gmail.com>
2014-07-07 18:55:48 +0100

    get error by status

--- lib/zss/error.rb
+++ lib/zss/error.rb
@@ -1,14 +1,34 @@
+require 'json'
+
 module ZSS
   class Error < ::StandardError
 
     attr_reader :code, :developer_message, :user_message
 
-    def initialize code, payload
+    def initialize(code, payload)
       @code = code.to_i
       @developer_message = payload.developerMessage
       @user_message = payload.userMessage
       super @developer_message
       set_backtrace caller
+    end
+
+    def self.[](code)
+      data = get_errors[code.to_s]
+      Error.new(code.to_i, data.body)
+    end
+
+    private
+
+    def self.get_errors
+      @errors ||= begin
+        path = File.join(
+          File.dirname(File.absolute_path(__FILE__)),
+          'errors.json'
+        )
+        file = File.read(path)
+        Hashie::Mash.new(JSON.parse(file))
+      end
     end
 
   end
--- spec/unit/error_spec.rb
+++ spec/unit/error_spec.rb
@@ -6,22 +6,35 @@ describe ZSS::Error do
     Hashie::Mash.new(userMessage: "user info", developerMessage: "dev info")
   end
 
-  subject do
-    described_class.new(500, response)
+  describe('#ctor') do
+
+    subject do
+      described_class.new(500, response)
+    end
+
+    it('returns a fullfilled error.message') do
+      expect(subject.code).to eq(500)
+      expect(subject.developer_message).to eq("dev info")
+      expect(subject.user_message).to eq("user info")
+    end
+
+    it('returns error with dev info as error.message') do
+      expect(subject.message).to eq("dev info")
+    end
+
+    it('returns error with stacktrace') do
+      expect(subject.backtrace).not_to be_nil
+    end
+
   end
 
-  it('returns a fullfilled error.message') do
-    expect(subject.code).to eq(500)
-    expect(subject.developer_message).to eq("dev info")
-    expect(subject.user_message).to eq("user info")
-  end
+  describe('.[]') do
 
-  it('returns error with dev info as error.message') do
-    expect(subject.message).to eq("dev info")
-  end
+    it('returns the error') do
+      result = described_class[500]
+      expect(result.code).to eq(500)
+    end
 
-  it('returns error with stacktrace') do
-    expect(subject.backtrace).not_to be_nil
   end
 
 end
