dc41a182502568ad15f33a86489fd5ee69afb047
Pedro Januário <prnjanuario@gmail.com>
2014-07-07 17:45:36 +0100

    added backend default config

--- lib/zss/configuration.rb
+++ lib/zss/configuration.rb
@@ -2,7 +2,10 @@ module ZSS
   class Configuration
 
     def self.default
-      Hashie::Mash.new frontend: 'tcp://127.0.0.1:5560'
+      Hashie::Mash.new(
+        frontend: 'tcp://127.0.0.1:5560',
+        backend:  'tcp://127.0.0.1:5559'
+      )
     end
 
   end
